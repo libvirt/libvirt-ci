libvirt/libvirt-ci

commit 182f2a029e8d8bde6c122dc1f725356f8cf2630f

guests: add a warning header to dockerfiles
Discourage people from editting the files manually.

If lcitool is being run from git, include the git commit hash that was
used to generate the package list.

Signed-off-by: Daniel P. Berrangé <berrange@redhat.com>

guests/lcitool/lcitool/formatters.py
@@ -274,8 +274,22 @@ class DockerfileFormatter(Formatter):
         self._projects = projects
         self._inventory = inventory
 
-    def _format_dockerfile(self, facts, cross_arch, varmap):
-        strings = []
+    def _format_dockerfile(self, host, project, facts, cross_arch, varmap):
+        cli_args = []
+        if cross_arch:
+            cli_args.extend("--cross", cross_arch)
+        cli_args.extend([host, project])
+        commit = util.git_commit()
+        url = "https://gitlab.com/libvirt/libvirt-ci"
+        if commit is not None:
+            url = url + "/-/commit/" + commit
+        strings = [
+            "# THIS FILE WAS AUTO-GENERATED",
+            "#",
+            "#  $ lcitool dockerfile %s" % " ".join(cli_args),
+            "#",
+            "# %s" % url
+        ]
 
         pkg_align = " \\\n" + (" " * len("RUN " + facts["packaging"]["command"] + " "))
         pypi_pkg_align = " \\\n" + (" " * len("RUN pip3 "))
@@ -520,7 +534,7 @@ class DockerfileFormatter(Formatter):
 
         facts, cross_arch, varmap = self._generator_prepare(args)
 
-        return '\n'.join(self._format_dockerfile(facts, cross_arch, varmap))
+        return '\n'.join(self._format_dockerfile(args.hosts, args.projects, facts, cross_arch, varmap))
 
 
 class VariablesFormatter(Formatter):

guests/lcitool/lcitool/util.py
@@ -6,9 +6,17 @@
 
 import fnmatch
 import platform
+import git
 
 from pkg_resources import resource_filename
 
+
+def git_commit():
+    try:
+        repo = git.Repo(search_parent_directories=True)
+        return repo.head.object.hexsha
+    except git.InvalidGitRepositoryError as ex:
+        return None
 
 def expand_pattern(pattern, source, name):
     if pattern is None:

commit 7c70c33c33ec01e6e865d829506e4fce18c08430

formatters: Fix a typo in cross arch dockerfile generator
Trying to generate dockerfiles for cross arches results in the following
traceback snippet:

File "libvirt-ci.git/guests/lcitool/lcitool/formatters.py", line 280, \
  in _format_dockerfile
    cli_args.extend("--cross", cross_arch)
TypeError: extend() takes exactly one argument (2 given)

Fixes: 182f2a029e8d8bde6c122dc1f725356f8cf2630f

Signed-off-by: Erik Skultety <eskultet@redhat.com>

guests/lcitool/lcitool/formatters.py
@@ -277,7 +277,7 @@ class DockerfileFormatter(Formatter):
     def _format_dockerfile(self, host, project, facts, cross_arch, varmap):
         cli_args = []
         if cross_arch:
-            cli_args.extend("--cross", cross_arch)
+            cli_args.extend(["--cross", cross_arch])
         cli_args.extend([host, project])
         commit = util.git_commit()
         url = "https://gitlab.com/libvirt/libvirt-ci"