libvirt/libvirt-ci

commit 97c4e7bdd3b0ac1af190bfaefdbcdea5e8a59d5f

guests: refactor iteration over internal project skip list
This provide better code formatting as we add more projects to the
list.

Signed-off-by: Daniel P. Berrangé <berrange@redhat.com>

guests/lcitool/lcitool/projects.py
@@ -52,7 +52,14 @@ class Projects:
 
         # Some projects are internal implementation details and should
         # not be exposed to the user
-        for project in ["base", "developer", "vm", "unwanted", "cloud-init"]:
+        internal_projects = [
+            "base",
+            "cloud-init"
+            "developer",
+            "unwanted",
+            "vm",
+        ]
+        for project in internal_projects:
             if project in projects:
                 projects.remove(project)
 

commit 18634e0ef61ef182fa2c08d0dc7b8804a54098e4

projects: Fix missing comma in the internal projects list
Fixes: 97c4e7bdd3b0ac1af190bfaefdbcdea5e8a59d5f

Signed-off-by: Erik Skultety <eskultet@redhat.com>

guests/lcitool/lcitool/projects.py
@@ -54,7 +54,7 @@ class Projects:
         # not be exposed to the user
         internal_projects = [
             "base",
-            "cloud-init"
+            "cloud-init",
             "developer",
             "perl-cpan",
             "python-pip",