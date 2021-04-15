libvirt/libvirt-ci

commit 87eac237a30179a44f23a70ca9a0bd8c303bb30e

logging: Scatter a handful of DEBUG messages around the code
This is a useful addition to error reporting. By default
all of these will end up on stderr, but should the need arise, we can
easily introduce a dedicated handler for other outputs as well (and
different formats for that matter).

Signed-off-by: Erik Skultety <eskultet@redhat.com>

guests/lcitool/lcitool/formatters.py
@@ -5,10 +5,14 @@
 # SPDX-License-Identifier: GPL-2.0-or-later
 
 import abc
+import logging
+import sys
 
 from pkg_resources import resource_filename
 
 from lcitool import util
+
+log = logging.getLogger(__name__)
 
 
 class Formatter(metaclass=abc.ABCMeta):
@@ -191,9 +195,15 @@ class Formatter(metaclass=abc.ABCMeta):
         if cpan_pkgs:
             varmap["cpan_pkgs"] = sorted(set(cpan_pkgs.values()))
 
+        log.debug(f"Generated varmap: {varmap}")
         return varmap
 
     def _generator_prepare(self, hosts, projects, cross_arch):
+        log.debug(f"Generating varmap for "
+                  f"hosts='{hosts}', "
+                  f"projects='{projects}', "
+                  f"cross_arch='{cross_arch}'")
+
         name = self.__class__.__name__.lower()
         mappings = self._projects.get_mappings()
         pypi_mappings = self._projects.get_pypi_mappings()
@@ -518,6 +528,9 @@ class DockerfileFormatter(Formatter):
         :returns: String represented Dockerfile
         """
 
+        log.debug(f"Generating Dockerfile for projects '{projects}' on host "
+                  f"'{hosts}' (cross_arch={cross_arch})")
+
         facts, cross_arch, varmap = self._generator_prepare(hosts, projects, cross_arch)
 
         return '\n'.join(self._format_dockerfile(hosts, projects, facts, cross_arch, varmap))
@@ -570,6 +583,9 @@ class VariablesFormatter(Formatter):
         :param args: Application class' command line arguments
         :returns: String represented list of environment variables
         """
+
+        log.debug(f"Generating variables for projects '{projects} on host "
+                  f"'{host}' (cross_arch={cross_arch})")
 
         _, _, varmap = self._generator_prepare(hosts, projects, cross_arch)
 

guests/lcitool/lcitool/application.py
@@ -5,6 +5,7 @@
 # SPDX-License-Identifier: GPL-2.0-or-later
 
 import json
+import logging
 import os
 import subprocess
 import sys
@@ -19,6 +20,8 @@ from lcitool.inventory import Inventory
 from lcitool.projects import Projects
 from lcitool.formatters import DockerfileFormatter, VariablesFormatter
 
+log = logging.getLogger(__name__)
+
 
 class Application:
 
@@ -32,6 +35,14 @@ class Application:
             sys.exit(1)
 
         self._native_arch = util.get_native_arch()
+
+    @staticmethod
+    def _entrypoint_debug(args):
+        cli_args = {}
+        for arg, val in vars(args).items():
+            if arg not in ['func', 'debug']:
+                cli_args[arg] = val
+        log.debug(f"Cmdline args={cli_args}")
 
     @staticmethod
     def _expand_pattern(obj, pattern):
@@ -52,6 +63,9 @@ class Application:
             sys.exit(1)
 
     def _execute_playbook(self, playbook, hosts, projects, git_revision):
+        log.debug(f"Executing playbook '{playbook}': hosts={hosts} "
+                  f"projects={projects} gitrev={git_revision}")
+
         base = resource_filename(__name__, "ansible")
         config = self._config
 
@@ -110,6 +124,7 @@ class Application:
         # rather than through the command line
         os.environ["ANSIBLE_CONFIG"] = ansible_cfg_path
 
+        log.debug(f"Running {cmd}")
         try:
             subprocess.check_call(cmd)
         except Exception as ex:
@@ -120,16 +135,22 @@ class Application:
             tempdir.cleanup()
 
     def _action_hosts(self, args):
+        self._entrypoint_debug(args)
+
         hosts_expanded = self._expand_pattern(self._inventory, "all")
         for host in hosts_expanded:
             print(host)
 
     def _action_projects(self, args):
+        self._entrypoint_debug(args)
+
         projects_expanded = self._expand_pattern(self._projects, "all")
         for project in projects_expanded:
             print(project)
 
     def _action_install(self, args):
+        self._entrypoint_debug(args)
+
         config = self._config
 
         try:
@@ -233,6 +254,7 @@ class Application:
             if not args.wait:
                 cmd.append("--noautoconsole")
 
+            log.debug(f"Running {cmd}")
             try:
                 subprocess.check_call(cmd)
             except Exception as ex:
@@ -242,14 +264,20 @@ class Application:
                 tempdir.cleanup()
 
     def _action_update(self, args):
+        self._entrypoint_debug(args)
+
         self._execute_playbook("update", args.hosts, args.projects,
                                args.git_revision)
 
     def _action_build(self, args):
+        self._entrypoint_debug(args)
+
         self._execute_playbook("build", args.hosts, args.projects,
                                args.git_revision)
 
     def _action_variables(self, args):
+        self._entrypoint_debug(args)
+
         hosts_expanded = self._expand_pattern(self._inventory, args.hosts)
         projects_expanded = self._expand_pattern(self._projects, args.projects)
 
@@ -268,6 +296,8 @@ class Application:
         print(header + variables)
 
     def _action_dockerfile(self, args):
+        self._entrypoint_debug(args)
+
         hosts_expanded = self._expand_pattern(self._inventory, args.hosts)
         projects_expanded = self._expand_pattern(self._projects, args.projects)
 

guests/lcitool/lcitool/config.py
@@ -4,11 +4,15 @@
 #
 # SPDX-License-Identifier: GPL-2.0-or-later
 
+import logging
 import os
+import sys
 import yaml
 
 from pathlib import Path
 from pkg_resources import resource_filename
+
+log = logging.getLogger(__name__)
 
 
 class Config:
@@ -31,6 +35,8 @@ class Config:
         else:
             return
 
+        user_config_path_str = user_config_path.as_posix()
+        log.debug(f"Loading configuration from '{user_config_path_str}'")
         try:
             with open(user_config_path, "r") as fp:
                 user_config = yaml.safe_load(fp)
@@ -61,6 +67,8 @@ class Config:
 
         for k in keys:
             if k not in known_keys:
+                log.debug(f"Removing unknown key '{k}' from config")
+
                 del _dict[k]
 
     def _remove_all_unknown_keys(self, config):
@@ -71,6 +79,9 @@ class Config:
                 self._remove_unknown_keys(config[section], self.values[section].keys())
 
     def _validate_section(self, section, mandatory_keys):
+        log.debug(f"Validating section='[{section}]' "
+                  f"against keys='{mandatory_keys}'")
+
         # check that the mandatory keys are present and non-empty
         for key in mandatory_keys:
             if self.values.get(section).get(key) is None:
@@ -101,4 +112,6 @@ class Config:
     def _update(self, values):
         for section in self.values.keys():
             if section in values:
+                log.debug(f"Applying user values: '{values[section]}'")
+
                 self.values[section].update(values[section])

guests/lcitool/lcitool/inventory.py
@@ -5,12 +5,16 @@
 # SPDX-License-Identifier: GPL-2.0-or-later
 
 import configparser
+import logging
+import sys
 import yaml
 
 from pathlib import Path
 from pkg_resources import resource_filename
 
 from lcitool import util
+
+log = logging.getLogger(__name__)
 
 
 class Inventory:
@@ -30,6 +34,8 @@ class Inventory:
         posix_path = Path("ansible", inventory_path).as_posix()
         inventory_path = resource_filename(__name__, posix_path)
         self._facts = {}
+
+        log.debug(f"Loading inventory '{inventory_path}'")
         try:
             # We can only deal with trivial inventories, but that's
             # all we need right now and we can expand support further
@@ -75,6 +81,8 @@ class Inventory:
                     continue
                 if yaml_path.suffix != ".yml":
                     continue
+
+                log.debug(f"Loading facts from '{yaml_path}'")
                 self._add_facts_from_file(facts, yaml_path)
 
         return facts

guests/lcitool/lcitool/projects.py
@@ -4,12 +4,16 @@
 #
 # SPDX-License-Identifier: GPL-2.0-or-later
 
+import logging
+import sys
 import yaml
 
 from pathlib import Path
 from pkg_resources import resource_filename
 
 from lcitool import util
+
+log = logging.getLogger(__name__)
 
 
 class Projects:
@@ -39,6 +43,7 @@ class Projects:
 
             project = item.stem
 
+            log.debug(f"Loading mapppings for project '{project}'")
             try:
                 with open(yaml_path, "r") as infile:
                     packages = yaml.safe_load(infile)

guests/lcitool/lcitool/util.py
@@ -6,11 +6,15 @@
 
 import fnmatch
 import git
+import logging
 import platform
+import sys
 import textwrap
 
 from pathlib import Path
 from pkg_resources import resource_filename
+
+log = logging.getLogger(__name__)
 
 
 def git_commit():
@@ -23,6 +27,9 @@ def git_commit():
 
 
 def expand_pattern(pattern, source, name):
+    collection = list(source.keys())
+    log.debug(f"Expanding {name} pattern '{pattern}' on '{collection}'")
+
     if pattern is None:
         raise Exception(f"Missing {name} list")
 

commit 6552fd8885423cfc383a58255eca542937f7d4ea

lcitool: Fix debug message
The current one causes

  $ lcitool variables freebsd-12 libvirt
  Failed to format variables: name 'host' is not defined

Fixes: 87eac237a30179a44f23a70ca9a0bd8c303bb30e
Signed-off-by: Andrea Bolognani <abologna@redhat.com>

guests/lcitool/lcitool/formatters.py
@@ -585,7 +585,7 @@ class VariablesFormatter(Formatter):
         """
 
         log.debug(f"Generating variables for projects '{projects} on host "
-                  f"'{host}' (cross_arch={cross_arch})")
+                  f"'{hosts}' (cross_arch={cross_arch})")
 
         _, _, varmap = self._generator_prepare(hosts, projects, cross_arch)
 