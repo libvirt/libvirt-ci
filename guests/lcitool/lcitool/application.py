libvirt/libvirt-ci

commit 1e9b58366986af6e06738f6ee703d9dd12353827

guests: don't pass cli args object into formatters
Pass the individual arguments into the formatters, instead of the cli
args object. This allows the formatters to be invoked from other
contexts where the cli object isn't available, giving separation
between the core implementation and the frontend interface invoking
it.

Signed-off-by: Daniel P. Berrangé <berrange@redhat.com>

guests/lcitool/lcitool/application.py
@@ -215,11 +215,16 @@ class Application:
                                args.git_revision)
 
     def _action_variables(self, args):
-        print(VariablesFormatter(self._projects, self._inventory).format(args))
+        print(VariablesFormatter(self._projects,
+                                 self._inventory).format(args.hosts,
+                                                         args.projects,
+                                                         None))
 
     def _action_dockerfile(self, args):
         print(DockerfileFormatter(self._projects,
-                                  self._inventory).format(args))
+                                  self._inventory).format(args.hosts,
+                                                          args.projects,
+                                                          args.cross_arch))
 
     def run(self, args):
         args.func(self, args)

guests/lcitool/lcitool/formatters.py
@@ -199,14 +199,14 @@ class Formatter(metaclass=abc.ABCMeta):
 
         return varmap
 
-    def _generator_prepare(self, args):
+    def _generator_prepare(self, hosts, projects, cross_arch):
         name = self.__class__.__name__.lower()
         mappings = self._projects.get_mappings()
         pypi_mappings = self._projects.get_pypi_mappings()
         cpan_mappings = self._projects.get_cpan_mappings()
         native_arch = util.get_native_arch()
 
-        hosts = self._inventory.expand_pattern(args.hosts)
+        hosts = self._inventory.expand_pattern(hosts)
         if len(hosts) > 1:
             raise Exception(
                 "Can't use '{}' use generator on multiple hosts".format(name)
@@ -214,7 +214,6 @@ class Formatter(metaclass=abc.ABCMeta):
         host = hosts[0]
 
         facts = self._inventory.get_facts(host)
-        cross_arch = args.cross_arch
 
         # We can only generate Dockerfiles for Linux
         if (name == "dockerfileformatter" and
@@ -245,7 +244,7 @@ class Formatter(metaclass=abc.ABCMeta):
                 raise Exception("Cross arch {} should differ from native {}".
                                 format(cross_arch, native_arch))
 
-        selected_projects = self._projects.expand_pattern(args.projects)
+        selected_projects = self._projects.expand_pattern(projects)
         for project in selected_projects:
             if project.rfind("+mingw") >= 0:
                 raise Exception("Obsolete syntax, please use --cross-arch")
@@ -530,7 +529,7 @@ class DockerfileFormatter(Formatter):
 
         return strings
 
-    def format(self, args):
+    def format(self, hosts, projects, cross_arch):
         """
         Generates and formats a Dockerfile.
 
@@ -543,9 +542,9 @@ class DockerfileFormatter(Formatter):
         :returns: String represented Dockerfile
         """
 
-        facts, cross_arch, varmap = self._generator_prepare(args)
+        facts, cross_arch, varmap = self._generator_prepare(hosts, projects, cross_arch)
 
-        return '\n'.join(self._format_dockerfile(args.hosts, args.projects, facts, cross_arch, varmap))
+        return '\n'.join(self._format_dockerfile(hosts, projects, facts, cross_arch, varmap))
 
 
 class VariablesFormatter(Formatter):
@@ -583,7 +582,7 @@ class VariablesFormatter(Formatter):
             strings.append("{}='{}'".format(name.upper(), value))
         return strings
 
-    def format(self, args):
+    def format(self, hosts, projects, cross_arch):
         """
         Generates and formats environment variables as KEY=VAL pairs.
 
@@ -595,6 +594,6 @@ class VariablesFormatter(Formatter):
         :returns: String represented list of environment variables
         """
 
-        _, _, varmap = self._generator_prepare(args)
+        _, _, varmap = self._generator_prepare(hosts, projects, cross_arch)
 
         return '\n'.join(self._format_variables(varmap))

commit 223902ea9d0915b56eda75f18b7d22f8d71d7b6a

lcitool: Fix cross-compilation support for variables command
This was work fine when the command was introduced, but a later
refactoring broke it.

Fixes: 1e9b58366986af6e06738f6ee703d9dd12353827
Signed-off-by: Andrea Bolognani <abologna@redhat.com>

guests/lcitool/lcitool/application.py
@@ -281,7 +281,7 @@ class Application(metaclass=Singleton):
 
         variables = VariablesFormatter().format(args.target,
                                                 projects_expanded,
-                                                None)
+                                                args.cross_arch)
 
         cliargv = [args.action]
         if args.cross_arch: