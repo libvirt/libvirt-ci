9297b032dd1d1c63fe7f974d35565224600959d4
Andrea Bolognani <abologna@redhat.com>
2021-09-30 14:43:54 +0200

    tests: Work with the entire dictionary
    This will make future patches easier, and also it results in
    having to tweak the data in a single place instead of two.
    
    Signed-off-by: Andrea Bolognani <abologna@redhat.com>

--- guests/lcitool/tests/test_packages.py
+++ guests/lcitool/tests/test_packages.py
@@ -45,16 +45,15 @@ def test_project():
 
 def test_verify_all_mappings_and_packages():
     expected_path = Path(DATA_DIR, "packages_in.yml")
-    actual = sorted(Projects().mappings["mappings"].keys())
+    actual = {"packages": sorted(Projects().mappings["mappings"].keys())}
 
     if pytest.custom_args["regenerate_output"]:
         with open(Path(DATA_DIR, "packages_in.yml"), "w") as fd:
-            yaml.safe_dump({"packages": actual}, stream=fd)
+            yaml.safe_dump(actual, stream=fd)
 
     # load the expected results
     with open(expected_path) as fd:
-        yaml_data = yaml.safe_load(fd)
-        expected = yaml_data["packages"]
+        expected = yaml.safe_load(fd)
 
     assert actual == expected
 
