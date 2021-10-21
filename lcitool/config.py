3619e96b40f8734ab051d18a52dee1d5afa06de2
Erik Skultety <eskultet@redhat.com>
2021-10-21 10:03:35 +0200

    config: Make '_config_file_paths' a private attribute
    Right now, we use it as an internal method, but the only thing the
    method does is that it returns a list of Path objects. The list only
    ever contained static data, so there's no reason why it couldn't be
    converted into a private instance attribute instead.
    
    Signed-off-by: Erik Skultety <eskultet@redhat.com>

--- lcitool/config.py
+++ lcitool/config.py
@@ -60,10 +60,10 @@ class Config(metaclass=Singleton):
 
     def __init__(self):
         self._values = None
-
-    def _config_file_paths(self):
-        return [Path(util.get_config_dir(), fname) for fname in
-                ["config.yml", "config.yaml"]]
+        self._config_file_paths = [
+            Path(util.get_config_dir(), fname) for fname in
+            ["config.yml", "config.yaml"]
+        ]
 
     def _load_config(self):
         # Load the template config containing the defaults first, this must
@@ -73,7 +73,7 @@ class Config(metaclass=Singleton):
             default_config = yaml.safe_load(fp)
 
         user_config_path = None
-        for user_config_path in self._config_file_paths():
+        for user_config_path in self._config_file_paths:
             if user_config_path.exists():
                 break
         else:
