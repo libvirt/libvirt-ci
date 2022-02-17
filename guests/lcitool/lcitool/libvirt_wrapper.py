ccb6568c65f284db6a75ae372460df20f74b1a61
Erik Skultety <eskultet@redhat.com>
2022-02-17 16:31:11 +0100

    guests: libvirt_wrapper: Introduce lcitool host listing
    This patch adds a method which queries all domains libvirt knows about
    and filters the ones which were created by lcitool - these will have a
    corresponding XML metadata entry.
    
    Signed-off-by: Erik Skultety <eskultet@redhat.com>

--- guests/lcitool/lcitool/libvirt_wrapper.py
+++ guests/lcitool/lcitool/libvirt_wrapper.py
@@ -8,6 +8,8 @@ import libvirt
 import logging
 import textwrap
 
+import xml.etree.ElementTree as xmlparser
+
 log = logging.getLogger(__name__)
 
 LCITOOL_XMLNS = "http://libvirt.org/schemas/lcitool/1.0"
@@ -36,6 +38,38 @@ class LibvirtWrapper():
         # Disable libvirt's default console error logging
         libvirt.registerErrorHandler(nop_error_handler, None)
         self._conn = libvirt.open()
+
+    @property
+    def hosts(self):
+        """Return all lcitool hosts."""
+
+        hosts = {}
+        try:
+            doms = self._conn.listAllDomains()
+        except libvirt.libvirtError as e:
+            raise LibvirtWrapper("Failed to load libvirt domains: " + e)
+
+        for dom in doms:
+            try:
+                xml = dom.metadata(libvirt.VIR_DOMAIN_METADATA_ELEMENT,
+                                   LCITOOL_XMLNS)
+            except libvirt.libvirtError as e:
+                if e.get_error_code() == libvirt.VIR_ERR_NO_DOMAIN_METADATA:
+                    # skip hosts which don't have lcitool's metadata
+                    continue
+
+                raise LibvirtWrapper(
+                    f"Failed to query metadata for '{dom.name}': " + str(e)
+                )
+
+            xmltree = xmlparser.fromstring(xml)
+            target = xmltree.find("target")
+            if xmltree.tag != "host" or target is None or target.text is None:
+                continue
+
+            hosts[dom.name()] = target.text
+
+        return hosts
 
     def set_target(self, host, target):
         """Inject target OS to host's XML metadata."""
