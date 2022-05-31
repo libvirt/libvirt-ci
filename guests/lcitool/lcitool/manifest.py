libvirt/libvirt-ci

commit 101f8c258fb65489bae0676e5603f7ac55d0e0b6

lcitool: Don't unconditionally reference sanity-checks.yml
If none of the sanity checks are enabled, then the file would
end up having zero length so lcitool doesn't create it. That
means we also have to avoid referencing it elsewhere.

Resolves: https://gitlab.com/libvirt/libvirt-ci/-/issues/14
Signed-off-by: Andrea Bolognani <abologna@redhat.com>

guests/lcitool/lcitool/manifest.py
@@ -304,9 +304,10 @@ class Manifest:
         if jobinfo["clang-format"]:
             testcontent.append(gitlab.clang_format_job())
 
-        path = Path(gitlabdir, "sanity-checks.yml")
-        self._replace_file(testcontent, path, dryrun)
-        includes.append(path)
+        if len(testcontent) > 0:
+            path = Path(gitlabdir, "sanity-checks.yml")
+            self._replace_file(testcontent, path, dryrun)
+            includes.append(path)
 
         if gitlabinfo["containers"]:
             path = Path(gitlabdir, "containers.yml")

commit e7290fa200b8362c9492af66679fee6cd7dcc039

lcitool: Fix handling of sanity-checks.yml
Manifest._replace_file() will not just not generate the file if
it would end up being empty, but will actively delete it. So we
should always call that function, and only make adding the file
to the list of includes conditional.

Fixes: 101f8c258fb65489bae0676e5603f7ac55d0e0b6
Signed-off-by: Andrea Bolognani <abologna@redhat.com>

guests/lcitool/lcitool/manifest.py
@@ -304,9 +304,9 @@ class Manifest:
         if jobinfo["clang-format"]:
             testcontent.append(gitlab.clang_format_job())
 
+        path = Path(gitlabdir, "sanity-checks.yml")
+        self._replace_file(testcontent, path, dryrun)
         if len(testcontent) > 0:
-            path = Path(gitlabdir, "sanity-checks.yml")
-            self._replace_file(testcontent, path, dryrun)
             includes.append(path)
 
         if gitlabinfo["containers"]: