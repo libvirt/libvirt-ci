335fab62d17720e970d71f94214bfd82f0a4e045
Paolo Bonzini <pbonzini@redhat.com>
2022-07-27 15:27:50 +0200

    print descriptive messages when assertions fail
    For the comparison with "golden" files it is quite complicated to
    understand what is different between the actual output and the expected
    one.
    
    Instead of just "assert x == y", do the comparison recursively on
    lists/dictionaries/strings so that it is possible to have output like
    this:
    
    AssertionError: comparing against /home/pbonzini/work/upstream/libvirt-ci/tests/data/formatters/out/opensuse-leap-153-all-projects.Dockerfile, at line 191: expected '           python3-Pillow \\', got '           python39-base \\'
    
    AssertionError: comparing against /home/pbonzini/work/upstream/libvirt-ci/tests/data/packages/out/opensuse-leap-153.yml, at dictionary item pypi: expected 7 items, got 13, first extra element: 'numpy'
    
    AssertionError: comparing against /home/pbonzini/work/upstream/libvirt-ci/tests/data/packages/out/opensuse-leap-153.yml, at dictionary item pypi, at item 5: expected 'meson==0.56.0', got 'lxml'
    
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- tests/test_utils/utils.py
+++ tests/test_utils/utils.py
@@ -26,6 +26,69 @@ def test_data_indir(test_name):
     return Path(test_data_dir(test_name), "in")
 
 
+def format_err_msg(indices, err_msg):
+    if indices:
+        return ", ".join(indices) + ": " + err_msg
+    else:
+        return err_msg
+
+
+def assert_equal_list(actual, expected, indices, kind):
+    len_err = None
+    if len(actual) != len(expected):
+        # stash the error for later: printing the first different element
+        # has higher priority, but actual and expected will not be
+        # available later to print the error
+        len_err = format_err_msg(indices, f"expected {len(expected)} {kind}s, got {len(actual)}, ")
+        if len(actual) < len(expected):
+            len_err += f"first missing element: {repr(expected[len(actual)])}"
+            expected = expected[:len(actual)]
+        else:
+            len_err += f"first extra element: {repr(actual[len(expected)])}"
+            actual = actual[:len(expected)]
+
+    indices.append(None)
+    try:
+        n = 0
+        for i, j in zip(iter(actual), iter(expected)):
+            indices[-1] = f"at {kind} {n}"
+            assert_equal(i, j, indices)
+            n += 1
+    finally:
+        indices.pop()
+    if len_err:
+        raise AssertionError(len_err)
+
+
+def assert_equal(actual, expected, indices):
+    if not isinstance(actual, type(expected)):
+        raise AssertionError(format_err_msg(indices, f"expected {type(expected)}, got {type(actual)}"))
+
+    if isinstance(expected, list):
+        assert_equal_list(actual, expected, indices, "item")
+
+    elif isinstance(expected, str) and "\n" in expected:
+        actual_lines = actual.split("\n")
+        expected_lines = expected.split("\n")
+        assert_equal_list(actual_lines, expected_lines, indices, "line")
+
+    elif isinstance(expected, dict):
+        actual_keys = sorted(actual.keys())
+        expected_keys = sorted(expected.keys())
+        assert_equal_list(actual_keys, expected_keys, indices, "key")
+
+        indices.append(None)
+        try:
+            for i in actual_keys:
+                indices[-1] = f"at key {i}"
+                assert_equal(actual[i], expected[i], indices)
+        finally:
+            indices.pop()
+
+    elif actual != expected:
+        raise AssertionError(format_err_msg(indices, f"expected {repr(expected)}, got {repr(actual)}"))
+
+
 def assert_yaml_matches_file(actual, expected_path, allow_regenerate=True):
     if pytest.custom_args["regenerate_output"] and allow_regenerate:
         # Make sure the target directory exists, since creating the
@@ -37,9 +100,7 @@ def assert_yaml_matches_file(actual, expected_path, allow_regenerate=True):
     with open(expected_path) as fd:
         expected = yaml.safe_load(fd)
 
-    assert actual.keys() == expected.keys()
-    for key in actual.keys():
-        assert actual[key] == expected[key]
+    assert_equal(actual, expected, [f"comparing against {expected_path}"])
 
 
 def assert_matches_file(actual, expected_path, allow_regenerate=True):
@@ -53,4 +114,4 @@ def assert_matches_file(actual, expected_path, allow_regenerate=True):
     with open(expected_path) as fd:
         expected = fd.read()
 
-    assert actual == expected
+    assert_equal(actual, expected, [f"comparing against {expected_path}"])
