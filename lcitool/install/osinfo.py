b3d1098faf71c4e7dd7d93e5de40f8fba0520c0a
Erik Skultety <eskultet@redhat.com>
2022-11-18 11:54:47 +0100

    install: osinfo: Implement OSInfoObject helper getters
    In order to get the OSInfo GObject we need to query libosinfo with the
    URL id (or go an extra mile by using the short ID which is more
    convenient but harder to query). Add helpers for this.
    
    Signed-off-by: Erik Skultety <eskultet@redhat.com>

--- lcitool/install/osinfo.py
+++ lcitool/install/osinfo.py
@@ -20,6 +20,18 @@ class OSinfoDB():
         loader.process_default_path()
         self._db = loader.get_db()
 
+    def get_os_by_id(self, libosinfo_id):
+        osinfo = self._db.get_os(libosinfo_id)
+        return OSinfoObject(osinfo)
+
+    def get_os_by_short_id(self, short_id):
+        _filter = Libosinfo.Filter()
+        _filter.add_constraint(Libosinfo.PRODUCT_PROP_SHORT_ID, short_id)
+
+        oslist = self._db.get_os_list()
+        filtered = Libosinfo.List.new_filtered(oslist, _filter).get_elements()
+        return OSinfoObject(filtered[0])
+
 
 class OSinfoObject(OSinfoAbstractObject):
     def __init__(self, obj):
