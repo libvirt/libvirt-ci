libvirt/libvirt-ci

commit be6a054f32991ff14ec633343c72d5b669ed04fc

projects: pass target object to get_packages
Pass the target object instead of facts + cross_arch.  Later on,
the target object will also mediate between a new class to store
the package mappings and Projects.

Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

lcitool/application.py
@@ -102,12 +102,12 @@ class Application:
 
         for host in hosts_expanded:
             facts = inventory.host_facts[host]
-            target = facts["target"]
+            target = inventory.get_target(facts["target"])
 
             # packages are evaluated on a target level and since the
             # host->target mapping is N-1, we can skip hosts belonging to a
             # target group for which we already evaluated the package list
-            if target in group_vars:
+            if target.name in group_vars:
                 continue
 
             # resolve the package mappings to actual package names
@@ -116,9 +116,9 @@ class Application:
                 internal_wanted_projects.append("cloud-init")
 
             selected_projects = internal_wanted_projects + projects_expanded
-            pkgs_install = projects.get_packages(selected_projects, facts)
-            pkgs_early_install = projects.get_packages(["early_install"], facts)
-            pkgs_remove = projects.get_packages(["unwanted"], facts)
+            pkgs_install = projects.get_packages(selected_projects, target)
+            pkgs_early_install = projects.get_packages(["early_install"], target)
+            pkgs_remove = projects.get_packages(["unwanted"], target)
             package_names = package_names_by_type(pkgs_install)
             package_names_remove = package_names_by_type(pkgs_remove)
             package_names_early_install = package_names_by_type(pkgs_early_install)
@@ -132,7 +132,7 @@ class Application:
             packages["early_install_packages"] = package_names_early_install["native"]
 
             group_vars[target] = packages
-            group_vars[target].update(inventory.target_facts[target])
+            group_vars[target].update(target.facts)
 
         ansible_runner.prepare_env(playbookdir=playbook_base,
                                    inventories=[inventory.ansible_inventory],

lcitool/formatters.py
@@ -79,10 +79,10 @@ class Formatter(metaclass=abc.ABCMeta):
         # projects are not resolved via the public API, so it requires special
         # handling
         pkgs = {}
-        pkgs.update(projects.internal["base"].get_packages(target.facts, target.cross_arch))
+        pkgs.update(projects.internal["base"].get_packages(target))
 
         # we can now load packages for the rest of the projects
-        pkgs.update(projects.get_packages(selected_projects, target.facts, target.cross_arch))
+        pkgs.update(projects.get_packages(selected_projects, target))
         package_names = package_names_by_type(pkgs)
 
         varmap = {

lcitool/projects.py
@@ -108,7 +108,7 @@ class Projects:
             log.debug(f"Failed to expand '{pattern}'")
             raise ProjectError(f"Failed to expand '{pattern}': {ex}")
 
-    def get_packages(self, projects, facts, cross_arch=None):
+    def get_packages(self, projects, target):
         packages = {}
 
         for proj in projects:
@@ -116,18 +116,18 @@ class Projects:
                 obj = self.public[proj]
             except KeyError:
                 obj = self.internal[proj]
-            packages.update(obj.get_packages(facts, cross_arch))
+            packages.update(obj.get_packages(target))
 
         return packages
 
-    def eval_generic_packages(self, facts, cross_arch, generic_packages):
+    def eval_generic_packages(self, target, generic_packages):
         pkgs = {}
-        factory = PackageFactory(self.mappings, facts)
+        factory = PackageFactory(self.mappings, target.facts)
         needs_pypi = False
         needs_cpan = False
 
         for mapping in generic_packages:
-            pkg = factory.get_package(mapping, cross_arch)
+            pkg = factory.get_package(mapping, target.cross_arch)
             if pkg is None:
                 continue
             pkgs[pkg.mapping] = pkg
@@ -142,10 +142,10 @@ class Projects:
         # projects
         if needs_pypi:
             proj = self.internal["python-pip"]
-            pkgs.update(proj.get_packages(facts, cross_arch))
+            pkgs.update(proj.get_packages(target))
         if needs_cpan:
             proj = self.internal["perl-cpan"]
-            pkgs.update(proj.get_packages(facts, cross_arch))
+            pkgs.update(proj.get_packages(target))
 
         return pkgs
 
@@ -185,21 +185,21 @@ class Project:
             log.debug(f"Can't load pacakges for '{self.name}'")
             raise ProjectError(f"Can't load packages for '{self.name}': {ex}")
 
-    def get_packages(self, facts, cross_arch=None):
-        osname = facts["os"]["name"]
-        osversion = facts["os"]["version"]
+    def get_packages(self, target):
+        osname = target.facts["os"]["name"]
+        osversion = target.facts["os"]["version"]
         target_name = f"{osname.lower()}-{osversion.lower()}"
-        if cross_arch is None:
+        if target.cross_arch is None:
             target_name = f"{target_name}-x86_64"
         else:
             try:
-                util.validate_cross_platform(cross_arch, osname)
+                util.validate_cross_platform(target.cross_arch, osname)
             except ValueError as ex:
                 raise ProjectError(ex)
-            target_name = f"{target_name}-{cross_arch}"
+            target_name = f"{target_name}-{target.cross_arch}"
 
         # lazy evaluation + caching of package names for a given distro
         if self._target_packages.get(target_name) is None:
-            self._target_packages[target_name] = self.projects.eval_generic_packages(facts, cross_arch,
+            self._target_packages[target_name] = self.projects.eval_generic_packages(target,
                                                                                      self.generic_packages)
         return self._target_packages[target_name]

tests/test_packages.py
@@ -85,8 +85,8 @@ def test_package_resolution(inventory, test_project, target, arch):
     else:
         outfile = f"{target}-cross-{arch}.yml"
     expected_path = Path(test_utils.test_data_outdir(__file__), outfile)
-    pkgs = test_project.get_packages(inventory.target_facts[target],
-                                     cross_arch=arch)
+    target_obj = inventory.get_target(target, arch)
+    pkgs = test_project.get_packages(target_obj)
     actual = packages_as_dict(pkgs)
 
     test_utils.assert_yaml_matches_file(actual, expected_path)
@@ -98,8 +98,8 @@ def test_package_resolution(inventory, test_project, target, arch):
 )
 def test_unsupported_cross_platform(inventory, test_project, target):
     with pytest.raises(ProjectError):
-        test_project.get_packages(inventory.target_facts[target],
-                                  cross_arch="s390x")
+        target_obj = inventory.get_target(target, "s390x")
+        test_project.get_packages(target_obj)
 
 
 @pytest.mark.parametrize(
@@ -111,8 +111,8 @@ def test_unsupported_cross_platform(inventory, test_project, target):
 )
 def test_cross_platform_arch_mismatch(inventory, test_project, target, arch):
     with pytest.raises(ProjectError):
-        test_project.get_packages(inventory.target_facts[target],
-                                  cross_arch=arch)
+        target_obj = inventory.get_target(target, arch)
+        test_project.get_packages(target_obj)
 
 
 @total_ordering

tests/test_projects.py
@@ -34,9 +34,8 @@ def project(request, projects):
 
 
 def test_project_packages(inventory, project):
-    target = inventory.targets[0]
-    facts = inventory.target_facts[target]
-    project.get_packages(facts)
+    target = inventory.get_target(inventory.targets[0])
+    project.get_packages(target)
 
 
 def test_project_package_sorting(project):

commit 0f5ced5e98373e8e102a5f9a78908b7d912cbe18

lcitool: Fix broken Ansible playbook run executions
Recent changes introducing a new object Target broke Ansible executions
as we started to index the group_vars dictionary with object IDs
instead of target names.

Fixes: be6a054f32991ff14ec633343c72d5b669ed04fc

Signed-off-by: Erik Skultety <eskultet@redhat.com>

lcitool/application.py
@@ -131,8 +131,8 @@ class Application:
             packages["unwanted_packages"] = package_names_remove["native"]
             packages["early_install_packages"] = package_names_early_install["native"]
 
-            group_vars[target] = packages
-            group_vars[target].update(target.facts)
+            group_vars[target.name] = packages
+            group_vars[target.name].update(target.facts)
 
         ansible_runner.prepare_env(playbookdir=playbook_base,
                                    inventories=[inventory.ansible_inventory],