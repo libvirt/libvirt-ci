aae5d3e69b76cf63c2d2fa0b33df6c7be7ae76be
Abdulwasiu Apalowo <abdulwasiuapalowo@gmail.com>
2022-12-21 16:25:33 +0100

    containers: docker: Override "_images" method
    Deserialize string in JSON format to Python list.
    
    Signed-off-by: Abdulwasiu Apalowo <abdulwasiuapalowo@gmail.com>

--- lcitool/containers/docker.py
+++ lcitool/containers/docker.py
@@ -1,3 +1,4 @@
+import json
 import logging
 
 from .containers import Container, ContainerError
@@ -28,3 +29,20 @@ class Docker(Container):
         super().__init__()
         self._run_exception = DockerRunError
         self._build_exception = DockerBuildError
+
+    def _images(self):
+        """
+        Get all container images.
+
+        :returns: a list of image details
+        """
+
+        img = super()._images()
+
+        # Docker lacks proper JSON format output and instead of a single JSON
+        # list object of all images it will return individual JSON objects
+        # for all images, one per line
+        images = [json.loads(image) for image in img.strip().split("\n")]
+
+        log.debug(f"Deserialized {self.engine} images\n%s", images)
+        return images
