2187970b37717f5b1721e4f01d51b152a802c204
Michal Privoznik <mprivozn@redhat.com>
2023-06-08 09:48:27 +0200

    Add OpenSUSE Leap 15.5
    New minor version of OpenSUSE Leap was released. Update our
    facts and switch gitlab-ci job. It ships python-3.11 so update
    our mappings too.
    
    Signed-off-by: Michal Privoznik <mprivozn@redhat.com>

--- ci/gitlab/all_mappings_prep_env/opensuse-prep.sh
+++ ci/gitlab/all_mappings_prep_env/opensuse-prep.sh
@@ -3,9 +3,9 @@
 zypper dist-upgrade -y
 zypper install -y \
     perl-App-cpanminus \
-    python310-base \
-    python310-pip
+    python311-base \
+    python311-pip
 
 # OpenSUSE doesn't create/refresh the python3 symlink to always point to the
 # newest python3 install available, it always points to 3.6 - FAIL
-update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.10 1
+update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.11 1
--- tests/data/formatters/out/libvirt-go-xml-module-opensuse-leap-15.Dockerfile
+++ tests/data/formatters/out/libvirt-go-xml-module-opensuse-leap-15.Dockerfile
@@ -1,4 +1,4 @@
-FROM registry.opensuse.org/opensuse/leap:15.4
+FROM registry.opensuse.org/opensuse/leap:15.5
 
 RUN zypper update -y && \
     zypper install -y \
