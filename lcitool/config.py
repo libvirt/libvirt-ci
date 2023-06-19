libvirt/libvirt-ci

commit dc74805efebd43a2120a26499822641c70e96fc0

config: Allow empty config
This is the final piece in allowing users to completely rely on default
values when dealing with VMs. All checks for empty user values are
dropped in this patch as we can be certain to always have our defaults
in place after the configs are merged together.

Signed-off-by: Erik Skultety <eskultet@redhat.com>

lcitool/config.py
@@ -92,7 +92,7 @@ class Config:
             raise LoadError(f"'{user_config_path.name}': {e}")
 
         if user_config is None:
-            raise ValidationError(f"'{user_config_path.name}' is empty")
+            user_config = {}
 
         # delete user params we don't recognize
         self._sanitize_values(user_config, default_config)
@@ -145,10 +145,6 @@ class Config:
                 raise ValidationError(f"Invalid type for key '{pathprefix}.{key}'")
 
     def _validate(self, values):
-        if values is None:
-            paths = ", ".join([str(p) for p in self._config_file_paths])
-            raise ValidationError(f"Missing or empty configuration file, tried {paths}")
-
         self._validate_keys(values)
 
         flavor = values["install"].get("flavor")

tests/test_config.py
@@ -15,6 +15,7 @@ from lcitool.config import ValidationError
 @pytest.mark.parametrize(
     "config_filename",
     [
+        "empty.yml",
         "full.yml",
         "minimal.yml",
         "minimal_no_root_password.yml",
@@ -32,7 +33,6 @@ def test_config(assert_equal, config, config_filename):
 @pytest.mark.parametrize(
     "config_filename",
     [
-        "empty.yml",
         "missing_gitlab_section_with_gitlab_flavor.yml",
         "root_password_none.yml",
     ],

tests/data/config/out/empty.yml
@@ -1,0 +1,17 @@
+gitlab:
+  runner_secret: NONE
+  url: https://gitlab.com
+install:
+  arch: x86_64
+  cloud_init: false
+  cpu_model: host-passthrough
+  disk_size: 15
+  flavor: test
+  machine: pc
+  memory_size: 2
+  network: default
+  root_password: root
+  ssh_key: ~/.ssh/id_rsa.pub
+  storage_pool: default
+  vcpus: 2
+  virt_type: kvm

commit aa7484e950645ad04ea6d988445ebe3f4cab4d3c

config: Permit no user config on VM installs
Commit dc74805e seemingly allowed usage of lcitool's install feature
without providing any user config and instead rely on default values.
That's nice and it kinda works, but only when an actual config.yml file
exists. If it doesn't, we just return from the config loading method
returning None which is wrong. This patch fixes it properly, so users
truly don't need to provide a config.yml if our defaults are enough.
A corresponding test case is provided this time.

Fixes: dc74805efebd43a2120a26499822641c70e96fc0

Signed-off-by: Erik Skultety <eskultet@redhat.com>

lcitool/config.py
@@ -78,20 +78,21 @@ class Config:
 
         user_config_path = None
         for user_config_path in self._config_file_paths:
-            if user_config_path.exists():
-                break
+            if not user_config_path.exists():
+                continue
+
+            user_config_path_str = user_config_path.as_posix()
+            log.debug(f"Loading configuration from '{user_config_path_str}'")
+            try:
+                with open(user_config_path, "r") as fp:
+                    user_config = yaml.safe_load(fp)
+                    if user_config is None:
+                        user_config = {}
+            except Exception as e:
+                raise LoadError(f"'{user_config_path.name}': {e}")
+
+            break
         else:
-            return
-
-        user_config_path_str = user_config_path.as_posix()
-        log.debug(f"Loading configuration from '{user_config_path_str}'")
-        try:
-            with open(user_config_path, "r") as fp:
-                user_config = yaml.safe_load(fp)
-        except Exception as e:
-            raise LoadError(f"'{user_config_path.name}': {e}")
-
-        if user_config is None:
             user_config = {}
 
         # delete user params we don't recognize

tests/data/config/out/no_config
@@ -1,4 +1,5 @@
 gitlab:
+  runner_secret: NONE
   url: https://gitlab.com
 install:
   arch: x86_64
@@ -9,8 +10,8 @@ install:
   machine: pc
   memory_size: 2
   network: default
-  root_password: foo
-  ssh_key: '~/.ssh/id_rsa.pub'
+  root_password: root
+  ssh_key: ~/.ssh/id_rsa.pub
   storage_pool: default
   vcpus: 2
   virt_type: kvm

tests/test_config.py
@@ -19,6 +19,7 @@ from lcitool.config import ValidationError
         "full.yml",
         "minimal.yml",
         "minimal_no_root_password.yml",
+        "no_config",
         "unknown_section.yml",
         "unknown_key.yml",
     ],