libvirt/libvirt-ci

commit 1b7a6793fc679a564cf098fcd1c75844ee07aad1

libvirt_wrapper: Add a convenience method to create a storage volume
Libvirt's interface is mainly XML driven which our callers shouldn't
really be concerned about. Instead, just expose XML tunables via method
arguments and deal with XMLs transparently within this module only.

Signed-off-by: Erik Skultety <eskultet@redhat.com>

lcitool/libvirt_wrapper.py
@@ -143,3 +143,38 @@ class LibvirtPoolObject(LibvirtAbstractObject):
         if self._path is None:
             self._path = self._get_xml_node("target/path")
         return Path(self._path)
+
+    def create_volume(self, name, capacity, allocation=None, _format=None,
+                      owner=None, group=None, mode=None,):
+
+        # define a base XML template to be updated depending on other params
+        volume_xml = textwrap.dedent(
+            f"""
+            <volume>
+              <name>{name}</target>
+              <capacity>{capacity}</capacity>
+            </volume>
+            """)
+
+        root_el = ET.fromstring(volume_xml)
+
+        if allocation:
+            allocation_el = ET.SubElement(root_el, "allocation")
+            allocation_el.text = allocation
+
+        if _format:
+            target_el = ET.SubElement(root_el, "target")
+            ET.SubElement(target_el, "format", {"type": _format})
+
+        if any(owner, group, mode):
+            target_el = ET.SubElement(root_el, "target")
+            perms_el = ET.SubElement(target_el, "permissions")
+            for perm_var, perm in [(owner, "owner"),
+                                   (group, "group"),
+                                   (mode, "mode")]:
+                if perm_var:
+                    node_el = ET.SubElement(perms_el, perm)
+                    node_el.text = perm_var
+
+        volume_xml = ET.dump(root_el)
+        self.raw.createXML(volume_xml)

commit 5da8807195994fec2f9aa0f4f19c724e17ee29e1

libvirt_wrapper: Fix how XML is dumped from element tree
Using the 'dump' method was a false assumption of commit 1b7a6793
because the method always dumps the resulting XML representation to
sys.stdout not to a string. Fix it.

Fixes: 1b7a6793fc679a564cf098fcd1c75844ee07aad1
Signed-off-by: Erik Skultety <eskultet@redhat.com>

lcitool/libvirt_wrapper.py
@@ -176,5 +176,5 @@ class LibvirtPoolObject(LibvirtAbstractObject):
                     node_el = ET.SubElement(perms_el, perm)
                     node_el.text = perm_var
 
-        volume_xml = ET.dump(root_el)
-        self.raw.createXML(volume_xml)
+        volume_xml = ET.tostring(root_el, encoding="UTF-8", method="xml")
+        self.raw.createXML(volume_xml.decode("UTF-8"))