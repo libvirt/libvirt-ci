libvirt/libvirt-ci

commit b4a37300cb00baf93ba012dd890a0e3e4880cdb0

application: Enable user-supplied Ansible pre-setup tasks
Officially allow users to pre-setup their lcitool machines by running
some basic tasks from <datadir>/ansible/. This is useful for
proprietary/enterprise distros.

Signed-off-by: Erik Skultety <eskultet@redhat.com>

lcitool/application.py
@@ -82,10 +82,19 @@ class Application:
         playbook_base = Path(base, "playbooks", playbook)
         group_vars = dict()
 
+        user_pre = False
+        if data_dir:
+            ansible_path = Path(data_dir.path, "ansible")
+            if ansible_path.exists():
+                if Path(ansible_path, "pre/tasks/main.yml").exists:
+                    user_pre = True
+
         extra_vars = config.values
         extra_vars.update({
             "base": base,
             "selected_projects": projects_expanded,
+            "user_datadir": str(data_dir.path) if data_dir else None,
+            "user_pre": user_pre,
         })
 
         log.debug("Preparing Ansible runner environment")

commit 8229423320f3d24a4389797f7d7ed1e5ff729520

application: Fix checking for user-supplied Ansible tasks file
pathlib.Path.exists is a function, but we were only checking the
attribute which naturally always resolves to True.

Fixes: b4a37300cb00baf93ba012dd890a0e3e4880cdb0
Signed-off-by: Erik Skultety <eskultet@redhat.com>

lcitool/application.py
@@ -86,7 +86,7 @@ class Application:
         if data_dir:
             ansible_path = Path(data_dir.path, "ansible")
             if ansible_path.exists():
-                if Path(ansible_path, "pre/tasks/main.yml").exists:
+                if Path(ansible_path, "pre/tasks/main.yml").exists():
                     user_pre = True
 
         extra_vars = config.values