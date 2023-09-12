libvirt/libvirt-ci

commit b7fd693b2700bc5136451ef88b190bb017658aee

Add exception handler for container
This patch introduces a couple of classes to
provide various error message for different
container action. "ContainerError" class is
subclassed to create these classes.

{Podman,Docker}RunError for error during
"{podman,docker} run".

{Podman,Docker}BuildError for error during
{podman,docker} build".

Exception instance attribute is also added to
Container, Podman and Docker.

Signed-off-by: Abdulwasiu Apalowo <abdulwasiuapalowo@gmail.com>

lcitool/containers/containers.py
@@ -2,7 +2,17 @@ import logging
 
 from abc import ABC
 
+from lcitool import LcitoolError
+
 log = logging.getLogger()
+
+
+class ContainerError(LcitoolError):
+    """Global exception type for this module."""
+
+    def __init__(self, message):
+        super().__init__(self)
+        self.message = self.__class__.__name__ + ": " + message
 
 
 class Container(ABC):
@@ -13,3 +23,6 @@ class Container(ABC):
             self.engine = None
         else:
             self.engine = self.__class__.__name__.lower()
+
+        self._run_exception = None
+        self._build_exception = None

lcitool/containers/__init__.py
@@ -1,5 +1,6 @@
 from .docker import Docker
 from .podman import Podman
+from .containers import ContainerError
 
 # this line only makes sense with 'from xyz import *'; it also silences flake8
-__all__ = ("Docker", "Podman")
+__all__ = ("Docker", "Podman", "ContainerError")

lcitool/containers/docker.py
@@ -1,10 +1,30 @@
 import logging
 
-from .containers import Container
+from .containers import Container, ContainerError
 
 log = logging.getLogger()
 
 
+class DockerBuildError(ContainerError):
+    """
+    Thrown whenever error occurs during
+    docker build operation.
+    """
+    pass
+
+
+class DockerRunError(ContainerError):
+    """
+    Thrown whenever error occurs during
+    docker run operation.
+    """
+    pass
+
+
 class Docker(Container):
     """Docker container class"""
-    pass
+
+    def __init__(self):
+        super().__init__()
+        self._run_exception = DockerRunError
+        self._build_exception = DockerBuildError

lcitool/containers/podman.py
@@ -1,10 +1,30 @@
 import logging
 
-from .containers import Container
+from .containers import Container, ContainerError
 
 log = logging.getLogger()
 
 
+class PodmanBuildError(ContainerError):
+    """
+    Thrown whenever error occurs during
+    podman build operation.
+    """
+    pass
+
+
+class PodmanRunError(ContainerError):
+    """
+    Thrown whenever error occurs during
+    podman run operation.
+    """
+    pass
+
+
 class Podman(Container):
     """Podman container class"""
-    pass
+
+    def __init__(self):
+        super().__init__()
+        self._run_exception = PodmanRunError
+        self._build_exception = PodmanBuildError

commit 1e57a7dcf73a2b41847bc935974c8e7ae3449c5f

containers: Fix constructor of ContainerError
Commit b7fd693b didn't follow the rest of the modules in terms of how a
per-module exception's constructor should be defined, accidentally
creating an infinite recursion in the chain of exception inheritance:
    *** RecursionError: maximum recursion depth exceeded while getting
        the repr of an object

    [ERROR]: An unexpected error occurred
    Traceback (most recent call last):
      File "<path>/libvirt-ci.git/lcitool/application.py", line 426, in run
        args.func(self, args)
      File "<path>/libvirt-ci.git/lcitool/application.py", line 415, in _action_container_run
        return self._container_run(params)
               ^^^^^^^^^^^^^^^^^^^^^^^^^^^
      File "<path>/libvirt-ci.git/lcitool/application.py", line 408, in _container_run
        return engine.run(**container_params)
               ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
      File "<path>/libvirt-ci.git/lcitool/containers/podman.py", line 26, in run
        return super().run(image, container_cmd, user, tempdir, env, datadir,
               ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
      File "<path>/libvirt-ci.git/lcitool/containers/containers.py", line 347, in run
      File "<path>/libvirt-ci.git/lcitool/containers/containers.py", line 289, in _run
        f"Image '{image}:{tag}' not found in local cache. "
    lcitool.containers.containers.ContainerError: <exception str() failed>

fixes: b7fd693b2700bc5136451ef88b190bb017658aee

Signed-off-by: Erik Skultety <eskultet@redhat.com>

lcitool/containers/containers.py
@@ -21,8 +21,7 @@ class ContainerError(LcitoolError):
     """Global exception type for this module."""
 
     def __init__(self, message):
-        super().__init__(self)
-        self.message = self.__class__.__name__ + ": " + message
+        super().__init__(message, "Container")
 
 
 class ContainerExecError(ContainerError):