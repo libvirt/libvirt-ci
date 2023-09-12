libvirt/libvirt-ci

commit 7fc18a6fb7f3d99de6d7907c5f06c5f7926787b0

application: Move the container image existence check to containers.py
This way it's actually more robust, because the check doesn't live only
in the application module, but in the container backend implementation
so whatever code imports from containers.py will benefit from the
check.

Signed-off-by: Erik Skultety <eskultet@redhat.com>

lcitool/containers/containers.py
@@ -282,6 +282,15 @@ class Container(ABC):
         pass
 
     def _run(self, image, container_cmd, engine_extra_args, **kwargs):
+        if ":" in image:
+            image, tag = image.split(":")
+
+        if not self.image_exists(image, tag):
+            raise ContainerError(
+                f"Image '{image}:{tag}' not found in local cache. "
+                "Build it or pull from registry first."
+            )
+
         cmd = [self.engine, "run"] + engine_extra_args
         cmd.extend([image, container_cmd])
 

lcitool/application.py
@@ -377,15 +377,6 @@ class Application:
                                                dir=util.get_temp_dir())
         params["tempdir"] = container_tempdir.name
 
-        image = args.image
-        tag = "latest"
-        if ":" in image:
-            image, tag = args.image.split(":")
-
-        if not client.image_exists(image, tag):
-            print(f"Image '{image}:{tag}' not found in local cache. Build it or pull from registry first.")
-            return
-
         params["image"] = args.image
         params["user"] = args.user
         if args.user.isdecimal():

commit f3212176f7e1711cc149b082206aa96a50c23636

containers: Assume 'latest' as the default tag
When commit 7fc18a6f moved the code in question from the application
module to the containers module, it missed an important bit - setting a
default value for the 'tag' variable which now causes:

    UnboundLocalError: cannot access local variable 'tag' where it is
    not associated with a value

fixes: 7fc18a6fb7f3d99de6d7907c5f06c5f7926787b0

Signed-off-by: Erik Skultety <eskultet@redhat.com>

lcitool/containers/containers.py
@@ -281,6 +281,7 @@ class Container(ABC):
         pass
 
     def _run(self, image, container_cmd, engine_extra_args, **kwargs):
+        tag = "latest"
         if ":" in image:
             image, tag = image.split(":")
 