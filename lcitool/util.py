libvirt/libvirt-ci

commit 409862fc8c6e66d16d5fcaac98231c384fd540e3

lcitool: remove obsolete RISC-V ports for Debian 13+ and add cross-compile check
Signed-off-by: yzewei <yangzewei@loongson.cn>

lcitool/util.py
@@ -409,10 +409,18 @@ class DataDir:
         return result
 
 
-def validate_cross_platform(cross_arch: str, osname: str) -> None:
+def validate_cross_platform(cross_arch: str, osname: str, osversion: str) -> None:
     if osname not in ["Debian", "Fedora"]:
         raise ValueError(f"Cannot cross compile on {osname}")
     if osname == "Debian" and cross_arch.startswith("mingw"):
         raise ValueError(f"Cannot cross compile for {cross_arch} on {osname}")
     if osname == "Fedora" and not cross_arch.startswith("mingw"):
         raise ValueError(f"Cannot cross compile for {cross_arch} on {osname}")
+    if (
+        osname == "Debian"
+        and cross_arch == "riscv64"
+        and int(osversion.split(".")[0]) < 13
+    ):
+        raise ValueError(
+            f"Cross compiling for {cross_arch} is not supported on Debian < 13"
+        )

lcitool/formatters.py
@@ -456,14 +456,6 @@ class BuildEnvFormatter(Formatter):
                     "dpkg --add-architecture {cross_arch_deb}",
                 ]
             )
-            if target.cross_arch == "riscv64":
-                cross_commands.extend(
-                    [
-                        "{nosync}{packaging_command} install debian-ports-archive-keyring",
-                        "{nosync}echo 'deb http://ftp.ports.debian.org/debian-ports/ sid main' > /etc/apt/sources.list.d/ports.list",
-                        "{nosync}echo 'deb http://ftp.ports.debian.org/debian-ports/ unreleased main' >> /etc/apt/sources.list.d/ports.list",
-                    ]
-                )
             cross_commands.extend(
                 [
                     "{nosync}{packaging_command} update",

lcitool/projects.py
@@ -270,7 +270,7 @@ class Project:
             target_name = f"{target_name}"
         else:
             try:
-                util.validate_cross_platform(target.cross_arch, osname)
+                util.validate_cross_platform(target.cross_arch, osname, osversion)
             except ValueError as ex:
                 raise ProjectError(str(ex))
             target_name = f"{target_name}-{target.cross_arch}-cross"

commit 4b6d2123650563a765be5a65908e12df171f218a

util: Fix Debian version detection in validate_cross_platform()
Not all Debian versions are numbers, there's also "Sid". Trying
to convert the string into an integer (using int() method) leads
to an exception. Well, skip Sid over.

Fixes: 409862fc8c6e66d16d5fcaac98231c384fd540e3
Fixes: c7a03f45a12c0328f61304370bc3be3cf56b2749
Signed-off-by: Michal Privoznik <mprivozn@redhat.com>

lcitool/util.py
@@ -419,6 +419,7 @@ def validate_cross_platform(cross_arch: str, osname: str, osversion: str) -> Non
     if (
         osname == "Debian"
         and cross_arch == "riscv64"
+        and osversion != "Sid"
         and int(osversion.split(".")[0]) < 13
     ):
         raise ValueError(
@@ -427,6 +428,7 @@ def validate_cross_platform(cross_arch: str, osname: str, osversion: str) -> Non
     if (
         osname == "Debian"
         and (cross_arch == "mipsel" or cross_arch == "mips64el")
+        and osversion != "Sid"
         and int(osversion.split(".")[0]) > 12
     ):
         raise ValueError(